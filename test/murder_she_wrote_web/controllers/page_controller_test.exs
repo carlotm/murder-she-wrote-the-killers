149be90a8085d7bd5f16b82c9a485c9e7a641830
Ascani Carlo <carlo.ratm@gmail.com>
2021-05-07 09:53:05 +0200

    Add 404 and 500 pages

--- test/murder_she_wrote_web/controllers/page_controller_test.exs
+++ test/murder_she_wrote_web/controllers/page_controller_test.exs
@@ -1,8 +1,10 @@
 defmodule MurderSheWroteWeb.PageControllerTest do
   use MurderSheWroteWeb.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "Visiting the homepage returns a 200 Status Code and contains right contents", %{
+    conn: conn
+  } do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "<h1>Murder She Wrote</h1>"
   end
 end
